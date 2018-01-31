5722d6e183b8833edf4b28b2e774991c87a1a9d3
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-01-31 13:51:33 -0700

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Taylor Bills on 1/31/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
