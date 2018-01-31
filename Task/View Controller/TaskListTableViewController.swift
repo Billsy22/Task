89469f8f774d82c3233563d982f621769653aeea
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-01-31 14:13:33 -0700

    Part One: View Hierarchy complete

---
+++ Task/View Controller/TaskListTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TaskListTableViewController.swift
+//  Task
+//
+//  Created by Taylor Bills on 1/31/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+import UIKit
+
+class TaskListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
