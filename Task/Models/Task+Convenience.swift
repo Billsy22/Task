dab7c3ac3db894ad7592ffeda9453c691b4daec4
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-01-31 14:33:06 -0700

    Part One: Implement Core Data Model complete

---
+++ Task/Models/Task+Convenience.swift
@@ -0,0 +1,21 @@
+//
+//  Task+Convenience.swift
+//  Task
+//
+//  Created by Taylor Bills on 1/31/18.
+//  Copyright © 2018 Taylor Bills. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Task {
+    
+    // MARK: -  Initializer
+    convenience init(name: String, notes: String = nil, due: Date = nil, context: NSManagedObjectContext = CoreDataStack.context) {
+        self.name = name
+        self.notes = notes
+        self.due = due
+        self.init(context: context)
+    }
+}
