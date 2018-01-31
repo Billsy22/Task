20c94b7d4b62eb1675f325e36fab7c5797892bee
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2018-01-31 15:36:22 -0700

    Part One: Basic Task List View and List View Editing complete

--- Task/View Controller/TaskListTableViewController.swift
+++ Task/View Controller/TaskListTableViewController.swift
@@ -10,7 +10,40 @@ import UIKit
 
 class TaskListTableViewController: UITableViewController {
 
+    // MARK: -  Life Cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    // MARK: -  Table View Data Source Functions
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TaskController.shared.tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        let task = TaskController.shared.tasks[indexPath.row]
+        cell.textLabel?.text = task.name
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let task = TaskController.shared.tasks[indexPath.row]
+            TaskController.shared.remove(task: task)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    // MARK: -  Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     }
 }
